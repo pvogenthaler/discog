aa6367143948e99ab41af895399e625a0d46bb3f
Paige <paige.vogenthaler@gmail.com>
2017-03-07 19:12:50 -0800

    Implement sync requests & promisify

---
+++ public/services/apiFactory.js
@@ -0,0 +1,81 @@
+var services = angular.module('discog.apiFactory', []);
+
+services.factory('ApiFactory', ['$http', function($http) {
+
+  var searchArtists = function (searchTerm) {
+
+    // use XMLHttpRequest for sync option & promisify
+    return new Promise(function (resolve, reject) {
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET', '/v1/api/artists/?name=' + searchTerm, false);
+
+      xhr.onload = function() {
+        if (this.status === 200) {
+          resolve(JSON.parse(xhr.response));
+        } else {
+          reject({
+            status: this.status,
+            statusText: xhr.statusText
+          })
+        }
+      }
+
+      xhr.onerror = function() {
+        console.log('onerror: ', xhr.status, shr.statusText)
+        reject({
+        status: this.status,
+        statusText: xhr.statusText
+      });
+      }
+      xhr.send();
+    });
+
+    // deprecated: $http doesn't support sync requests
+    // return $http({
+    //   method: 'GET',
+    //   url: '/v1/api/artists/?name=' + searchTerm,
+    // });
+
+  };
+
+  var retrieveAlbums = function (artistMbid) {
+
+    return new Promise(function (resolve, reject) {
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET', 'v1/api/albums/?artist_mbid=' + artistMbid, false);
+
+      xhr.onload = function() {
+        if (this.status === 200) {
+          resolve(JSON.parse(xhr.response));
+        } else {
+          reject({
+            status: this.status,
+            statusText: xhr.statusText
+          })
+        }
+      }
+
+      xhr.onerror = function() {
+        console.log('onerror: ', xhr.status, shr.statusText)
+        reject({
+        status: this.status,
+        statusText: xhr.statusText
+      });
+      }
+      xhr.send();
+    });
+
+    // deprecated: $http doesn't support sync requests
+    // return $http({
+    //   method: 'GET',
+    //   url: 'v1/api/albums/?artist_mbid=' + artistMbid,
+    // });
+
+  };
+
+  return {
+    searchArtists: searchArtists,
+    retrieveAlbums: retrieveAlbums,
+  }
+
+}]);
